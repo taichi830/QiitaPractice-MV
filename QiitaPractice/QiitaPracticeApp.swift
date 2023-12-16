541639f29cf8cf2855f14094b4b2e2e8a92ced6f
fukuda taichiro <taichi@fukudanoMacBook-Pro.local>
2023-12-16 15:16:45 +0900

    first commit

---
+++ QiitaPractice/QiitaPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  QiitaPracticeApp.swift
+//  QiitaPractice
+//
+//  Created by fukuda taichiro on 2023/12/16.
+//
+
+import SwiftUI
+
+@main
+struct QiitaPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
